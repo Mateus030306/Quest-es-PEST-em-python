eee09946190d8215fa472ea28ffc995aaae7285b
Mateus Oliveira Santos <nenen2358@gmail.com>
2026-02-28 21:27:32 -0300

    Lista 2 iniciada

---
+++ Listas/Statistics2.py
@@ -0,0 +1,7 @@
+from ..Bibliotecas.Lib1 import *
+
+def main() -> None:
+    dados = [16, 17, 18, 8, 14, 14, 10, 10, 14, 15, 15, 20, 12, 13, 17, 14, 14, 15, 16, 12]
+    Selection_Sort(dados)
+    print(dados)
+main()
